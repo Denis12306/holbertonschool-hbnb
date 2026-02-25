5c085c0c29122efb546e26509059be7a149c3c9d
ValPQT <valentinpasquiet@gmail.com>
2026-02-25 21:10:11 +0100

    fix : update the file façade.py

--- part2/app/services/facade.py
+++ part2/app/services/facade.py
@@ -3,6 +3,7 @@ from app.persistence.repository import InMemoryRepository
 Business Logic, and Persistence layers. You will interact with the repositories
 (like the in-memory repository) through this Class:"""
 from app.models.user import User
+from app.models.place import Place
 
 
 class HBnBFacade:
@@ -12,12 +13,8 @@ class HBnBFacade:
         self.review_repo = InMemoryRepository()
         self.amenity_repo = InMemoryRepository()
 
-    """PLACE"""
-    def get_place(self, place_id):
-        # Logic will be implemented in later tasks
-        pass
+    """User Methods"""
 
-    """USER"""
     def create_user(self, user_data):
         # Placeholder method for creating a user
         user = User(**user_data)
@@ -30,24 +27,6 @@ class HBnBFacade:
     def get_user_by_email(self, email):
         return self.user_repo.get_by_attribute('email', email)
 
-    def create_place(self, place_data):
-        # Placeholder for logic to create a place, including validation for price, latitude, and longitude
-        pass
-
-
-    def get_place(self, place_id):
-    # Placeholder for logic to retrieve a place by ID, including associated owner and amenities
-        pass
-
-
-    def get_all_places(self):
-    # Placeholder for logic to retrieve all places
-        pass
-
-
-    def update_place(self, place_id, place_data):
-    # Placeholder for logic to update a place
-        pass
     def get_all_users(self):
         return self.user_repo.get_all()
 
@@ -58,3 +37,33 @@ class HBnBFacade:
         for key, value in user_data.items():
             setattr(user, key, value)
             return user
+
+    """Place Methods"""
+
+    def create_place(self, place_data):
+        place = Place(**place_data)
+        self.place_repo.add(place)
+        return place
+
+    def get_place(self, place_id):
+        return self.place_repo.get(place_id)
+
+    def get_all_places(self):
+        return self.place_repo.get_all()
+
+    def update_place(self, place_id, place_data):
+        place = self.get_place(place_id)
+        if not place:
+            return None
+        """Liste des champs que l'on autorise à modifier"""
+        allowed_fields = ["title", "description",
+                          "price", "latitude", "longitude"]
+        for key, value in allowed_fields.items():
+            setattr(place, key, value)
+        """Le repo sauvegarde les changements"""
+        self.place_repo.update(place)
+        return place
+
+    """Amenity Methods"""
+
+    """Review Methods"""
