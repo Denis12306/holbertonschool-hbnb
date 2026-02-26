cb5d83a64435c20c0007e5d8b5f7aef662dbdead
Denis12306 <denis.gavaud@gmail.com>
2026-02-26 15:14:17 +0100

    AddAmenityendpoints

--- part2/app/services/facade.py
+++ part2/app/services/facade.py
@@ -3,6 +3,7 @@ from app.persistence.repository import InMemoryRepository
 Business Logic, and Persistence layers. You will interact with the repositories
 (like the in-memory repository) through this Class:"""
 from app.models.user import User
+from app.models.amenity import Amenity
 
 
 class HBnBFacade:
@@ -11,13 +12,17 @@ class HBnBFacade:
         self.place_repo = InMemoryRepository()
         self.review_repo = InMemoryRepository()
         self.amenity_repo = InMemoryRepository()
+        self.users = []
+        self.amenities = []
 
     """PLACE"""
+
     def get_place(self, place_id):
         # Logic will be implemented in later tasks
         pass
 
     """USER"""
+
     def create_user(self, user_data):
         # Placeholder method for creating a user
         user = User(**user_data)
@@ -40,3 +45,22 @@ class HBnBFacade:
         for key, value in user_data.items():
             setattr(user, key, value)
             return user
+
+    """AMENITY"""
+
+    def create_amenity(self, amenity_data):
+        new_amenity = Amenity(**amenity_data)
+        self.amenity_repo.add(new_amenity)
+        return new_amenity
+
+    def get_amenity(self, amenity_id):
+        return self.amenity_repo.get(amenity_id)
+
+    def get_all_amenities(self):
+        return self.amenity_repo.get_all()
+
+    def update_amenity(self, amenity_id, amenity_data):
+        updated = self.amenity_repo.update(amenity_id, amenity_data)
+        if updated is None:
+            raise ValueError("Amenity not found")
+        return updated
