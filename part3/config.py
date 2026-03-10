d9643a34214d3bc380442e7ddcb339104f111ef7
Valentin PASQUIET <valentinpasquiet@gmail.com>
2026-03-10 14:50:20 +0100

    fix : launch the server (#26)

--- part3/config.py
+++ part3/config.py
@@ -1,11 +1,13 @@
 import os
-"""Create the file where you can define environment-specific settings"""
+
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'default_secret_key')
     DEBUG = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 class DevelopmentConfig(Config):
     DEBUG = True
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///hbnb.db')
 
 config = {
     'development': DevelopmentConfig,
